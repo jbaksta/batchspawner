24032d3f4731f294b9e529286328c4f4791d17b3
Jared Baker <jbaker@cheyenne1.ib0.cheyenne.ucar.edu>
2019-01-21 11:13:31 -0700

    testing whether spawner is subclass

--- batchspawner/api.py
+++ batchspawner/api.py
@@ -2,6 +2,8 @@ import json
 from tornado import web
 from jupyterhub.apihandlers import  APIHandler, default_handlers
 
+from batchspawner import BatchSpawnerBase
+
 class BatchSpawnerAPIHandler(APIHandler):
     @web.authenticated
     def post(self):
@@ -9,7 +11,10 @@ class BatchSpawnerAPIHandler(APIHandler):
         user = self.get_current_user()
         data = self.get_json_body()
         port = int(data.get('port', 0))
-        user.spawner.current_port = port
+        spawner = user.spawner
+        while not issubclass(spawner,BatchSpawnerBase):
+          spawner = spawner.child_spawner
+        spawner.current_port = port
         self.finish(json.dumps({"message": "BatchSpawner port configured"}))
         self.set_status(201)
 
